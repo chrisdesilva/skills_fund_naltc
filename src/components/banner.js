f4dad10ed04cc63d9ec3128315463f30ad815e0a
Chris DeSilva <chris.a.desilva@gmail.com>
2019-09-30 15:20:31 -0500

    Fill in template with school info

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,7 +7,7 @@ const Banner = props => {
     <div className='flex flex-col justify-center items-center relative z-0' id='banner'>
         {/* update h2 caps words with school-specific content */}
         <h1 className='text-3xl lg:text-4xl font-normal text-center mt-8 text-black px-4'>{applicationsLive ? headline : `Applications for ${schoolName}'s next cohort will be available beginning ${nextCohortStartDate}`}</h1>
-        <h2 className='text-base lg:text-xl font-light text-center text-black'>Know exactly what you'll pay with Skills Fund</h2>
+        {applicationsLive && <h2 className='text-base lg:text-xl font-light text-center text-black'>Know exactly what you'll pay with Skills Fund</h2>}
         <div className='flex flex-col lg:flex-row pb-16'>
             <Button
                 buttonClassName='opacityApply uppercase bg-primary p-3 lg:mb-0 w-40 rounded-full shadow-lg text-white'
